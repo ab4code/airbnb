8c699de01a1f331047acc5a09a2d9944f1b82d2b
ab4code <amjustabraham@gmail.com>
2022-10-16 00:40:55 +0100

    added banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,13 @@ const Banner  = () =>{
     return(
         <nav className="banner">
             <img src={banner} alt="logo"/>
+            <div className="banner-text">
+                <p className="title">Online Experiences</p>
+                <p className="info">
+                    Join unique interactive activities led 
+                    by <br/> one-of-a-kind hosts—all without leaving <br/> home.
+                </p>
+            </div>
         </nav>
     )
 }
